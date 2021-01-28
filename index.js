7142b2bb4cd77eae379d870af4d37b24080642fd
=Rob Franke <robwfranke@gmail.com>
2021-01-28 13:28:08 +0100

    index legggemaakt

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 
 
 function checkValue(n1){
-
-    console.log("checkValue")
-    if (n1<5){
-        return answer = "kleiner dan 5"
-    }
-    if (n1>10){
-        return answer = "groter dan 10"
-    }
-    return answer = "5 tm 10"
+// succes!!~
 
 }
 
